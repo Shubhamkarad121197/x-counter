a411d2d829578d286fd1ba7e008d75b44bb21591
shubham karad <shubham.karad12@gmail.com>
2025-09-08 23:18:16 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  
+  const increment=()=> setCount(count+1);
+  const decrement=()=>setCount(count-1)
+
+  return (
+    <>
+      <div className='counterContainer'>
+        <div className='counter'>
+          <h1>Counter App</h1>
+        <span>Count:{count}</span>
+        <br />
+        <div>
+           <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+        </div>
+        </div>
+        
+       
+      </div>
+    </>
+  )
+}
+
+export default App
